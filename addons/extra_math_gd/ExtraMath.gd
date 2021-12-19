c7e35387899d99097884fff9b3ad5b0c9a828acb
Balint Kovari <balintmaci@gmail.com>
2021-12-19 16:32:45 +0100

    handle calc from or to zero vect

--- addons/extra_math_gd/ExtraMath.gd
+++ addons/extra_math_gd/ExtraMath.gd
@@ -26,6 +26,9 @@ static func get_vectors_rotation(from: Vector3, to: Vector3) -> Vector3:
     var dot = from.dot(to) # dot = |a|×|b|× cos(fi)
     var cross = from.cross(to) # cross = |a|×|b|× sin(fi) × n
     var length_product = from.length() * to.length()
+    if length_product == 0:
+        push_error("Cannot calculate rotation from or to ZERO vector.")
+        return Vector3.ZERO
     var cos_fi = dot / length_product
     var sin_fi = cross.length() / length_product
     var fi = atan2(sin_fi, cos_fi)
